d46d8700042dd2a184575ad54202a9ab96453c83
TFor <tforren@udel.edu>
2017-11-01 15:26:27 -0400

    started on second half of lab

--- tree.c
+++ tree.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <papi.h>
 
-struct p {
+int i;
+static unsigned len = 16*(1 << 20);
+ 
+typedef struct p {
   int v;
   struct p * left;
   struct p * right;
-};
+} p;
 
 
 struct p * newNode(int value) {
@@ -60,7 +64,7 @@ struct p * add_if_not_present(int v, struct p * somewhere) {
 
 int size(struct p * somewhere) {
   int c = 1;
-  if (node somewhere == NULL) {
+  if (somewhere == NULL) {
     return 0;
   } else {
     c += size(somewhere->left);
@@ -69,13 +73,13 @@ int size(struct p * somewhere) {
   }
 }
 
-
+/*
 int checkIntegrity(struct p * somewhere) {
   if (somewhere == NULL) {
     return(0);
-  } else if (somewhere->left != NULL && maxValue(somewhere->left) > somewhere->data) {
+  } else if (somewhere->left != NULL && maxValue(somewhere->left) > somewhere->v) {
     return(0);
-  } else if (somewhere->right != NULL && minValue(somewhere->right) < somewhere->data) {
+  } else if (somewhere->right != NULL && minValue(somewhere->right) < somewhere->v) {
     return(0);
   } else if (!checkIntegrity(somewhere->left) || !checkIntegrity(somewhere->right)) {
     return(0);
@@ -83,10 +87,34 @@ int checkIntegrity(struct p * somewhere) {
 
   return(1);
 }
+*/
 
+
+void baseline(struct p * tree) {
+  for(i = 0; i<len;i++) {
+    add(random(),tree);  
+  }
+}
+
+void seq(struct p * tree) {
+  for (i = 0; i<len;i++) {
+    add(i,tree);
+  }
+}
+
+
+void curFunc(struct p * tree) {
+  baseline(tree);
+  //seq(tree);
+};
+
+/* All that PAPI Goodness */ 
+
+int events[1] = {PAPI_L2_DCM};
+int eventnum = 1;
 
 int main() {
-  struct p *testTree = newNode(50);
+/*  struct p *testTree = newNode(50);
 
   add(25,testTree);
   add(15,testTree); 
@@ -96,5 +124,38 @@ int main() {
   
   printf("right %d\n",testTree->right->v); 
   find(20, testTree);
+*/
+
+  long long values[1];
+  int eventset;
+
+  if (PAPI_VER_CURRENT != PAPI_library_init(PAPI_VER_CURRENT)) {
+    printf("Can't initiate PAPI library!\n");
+    exit(-1);
+  }
+
+  eventset = PAPI_NULL;
+  if(PAPI_create_eventset(&eventset) != PAPI_OK) {
+    printf("can't create eventset!\n");
+    exit(-3);
+  }
+
+  if(PAPI_OK != PAPI_add_events(eventset, events, eventnum)) {
+    printf("can't add events!\n");
+    exit(-4);
+  }
+
+  int i;
+  struct p *tree;
+  tree = (p *) malloc(len*sizeof(p)); 
+  PAPI_start(eventset);
+  curFunc(tree);
+  PAPI_stop(eventset,values);
+  free(tree);
+ 
+  char event_name[PAPI_MAX_STR_LEN];
+  if (PAPI_event_code_to_name( events[0], event_name ) == PAPI_OK)
+	printf("%s: %lld\n", event_name, values[0]);
+ 
   return 0;
 }
